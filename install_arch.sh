f6edfc896d30f9212f65ed30f8ecf33f21204766
Lucas Silva <lucas.nascimento@gmail.com>
2025-05-06 16:17:10 -0700

    feat: clean up + added paru, wezterm and zen-browser

---
+++ install_arch.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+sudo pacman -S --noconfirm --needed git
+sudo pacman -S --noconfirm --needed zsh
+sudo pacman -S --noconfirm --needed nerd-fonts
+sudo pacman -S --noconfirm --needed wl-clipboard
+sudo pacman -S --noconfirm --needed lazygit
+
+sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
+
+if ! test -d $ZSH
+then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if ! test -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+fi
+# install paru
+
+if ! command -v paru 2>&1 >/dev/null
+then
+	sudo pacman -S --noconfirm --needed rustup, base-devel
+
+	rustup
+
+	git clone https://aur.archlinux.org/paru.git
+	cd paru && makepkg -si
+	cd ../ && rm -rf paru
+fi
+
+# install wezterm
+if ! command -v wezterm 2>&1 >/dev/null
+then
+	paru -S wezterm-git
+fi
+
+if ! command -v zen-browser 2>&1 >/dev/null
+then
+	paru -S zen-browser-bin
+fi
+
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S --noconfirm --needed git
-sudo pacman -S --noconfirm --needed zsh
-
-sudo chsh -s $(which zsh)
-chsh -s $(which zsh)
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
