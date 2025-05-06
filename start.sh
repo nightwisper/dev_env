f6e708f31e72be5dc16a47c87657b0addc9d964b
Lucas Silva <lucas.nascimento@gmail.com>
2025-05-06 15:01:03 -0700

    feat: zsh initial setup

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sudo pacman -S --noconfirm --needed git
+sudo pacman -S --noconfirm --needed zsh
+
+sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
