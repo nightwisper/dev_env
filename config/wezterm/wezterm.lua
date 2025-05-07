186ad0655ff36f6b30f058096c8e8c3a26af0a51
Lucas Silva <lucas.nascimento@gmail.com>
2025-05-06 20:06:30 -0700

    feat: added more wezterm configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,6 +2,11 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- config.font
+config.font = wezterm.font("MesloLGS Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font_size = 19
+config.enable_wayland = true
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
 
 return config
